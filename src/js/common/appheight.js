32f251510218569a1ff70f8500da88ae4b06aa41
Ищенко Дмитрий <dishchenko@diasoft.ru>
2023-02-22 17:59:31 +0300

    modal added

--- src/js/common/appheight.js
+++ src/js/common/appheight.js
@@ -4,3 +4,18 @@ const appHeight = () => {
 };
 window.addEventListener('resize', appHeight);
 appHeight();
+
+// ----------------
+const modal = document.querySelector('.modal');
+
+if (window.innerHeight < innerWidth || window.innerWidth > 540) {
+  modal.classList.add('active');
+}
+
+window.onresize = () => {
+  if (window.innerHeight < innerWidth || window.innerWidth > 540) {
+    modal.classList.add('active');
+  } else {
+    modal.classList.remove('active');
+  }
+};
